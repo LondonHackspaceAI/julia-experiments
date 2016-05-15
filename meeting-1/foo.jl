b2a7d8a197bb90330e6e5a64fec5d486da8d413e
Christian Jaeger <ch@christianjaeger.ch>
2016-05-15 20:19:22 +0100

    re-indent with julia emacs mode

--- meeting-1/foo.jl
+++ meeting-1/foo.jl
@@ -1,18 +1,18 @@
 
 function fact(n)
-	if n == 0
-		1
-	else
-		n * fact(n-1)
-	end
+    if n == 0
+	1
+    else
+	n * fact(n-1)
+    end
 end
 
 function fib(n)
-	if n < 2
-		1
-	else
-		fib(n-1) + fib(n-2)
-	end
+    if n < 2
+	1
+    else
+	fib(n-1) + fib(n-2)
+    end
 end
 
 n = parse(Int,ARGS[1])
