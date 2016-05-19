b0d4f79d863c2b79b2a5cf4782216ac9c6c5121d
Christian Jaeger <ch@christianjaeger.ch>
2016-05-19 00:50:46 +0000

    only spawn in first call (not working?)
     chris@borg:~/julia-experiments/meeting-1$ time julia foo.jl  15
     15
     987
     c:0
    
     real	0m1.704s
     user	0m2.448s
     sys	0m6.516s
     chris@borg:~/julia-experiments/meeting-1$ time julia -p 2 foo.jl  15
     15
     987
     c:0
    
     real	0m10.088s
     user	0m18.388s
     sys	0m7.912s
     chris@borg:~/julia-experiments/meeting-1$ time julia -p 64 foo.jl  15
     15
     987
     c:0
    
     real	0m36.230s
     user	1m43.968s
     sys	6m45.260s

--- meeting-1/foo.jl
+++ meeting-1/foo.jl
@@ -9,20 +9,23 @@ end
 
 c=0
 
-@everywhere function fib(n)
+@everywhere function _fib(n, is_firstcall)
+    fib_(n)= _fib(n, false)
     #global c=c+1
     if n < 2
 	1
     else
-        if (1==0)
-            fib(n-1) + fib(n-2)
+        if (!(is_firstcall))
+            fib_(n-1) + fib_(n-2)
         else
-            a= @spawn fib(n-1)
-            b= @spawn fib(n-2)
+            a= @spawn fib_(n-1)
+            b= @spawn fib_(n-2)
             fetch(a) + fetch(b)
         end
     end
 end
+
+fib(n)= _fib(n, true)
 
 n = parse(Int,ARGS[1])
 println(n)
