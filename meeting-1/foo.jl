58c7150613bddc86b42c45d206251ff5f0aa9b95
Christian Jaeger <ch@christianjaeger.ch>
2016-05-19 01:16:16 +0000

    version that should spawn exactly 64 times. *Extremely* slow.
     chris@borg:~/julia-experiments/meeting-1$ time julia -O foo.jl  34
    
    is still running after >8'

--- meeting-1/foo.jl
+++ meeting-1/foo.jl
@@ -9,13 +9,13 @@ end
 
 c=0
 
-@everywhere function _fib(n::Int, already_spawned::Bool)
-    fib_(n::Int)= _fib(n, true)
+@everywhere function _fib(n::Int, level::Int)
+    fib_(n::Int)= _fib(n, level+1)
     #global c=c+1
     if n < 2
 	1
     else
-        if (already_spawned)
+        if (level<6) # 2**6==64 cores
             fib_(n-1) + fib_(n-2)
         else
             a= @spawn fib_(n-1)
@@ -25,7 +25,7 @@ c=0
     end
 end
 
-fib(n)= _fib(n, false)
+fib(n)= _fib(n, 0)
 
 n = parse(Int,ARGS[1])
 println(n)
