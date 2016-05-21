4e58c865918cd82346b46a62777bf4ac6de3e1d8
Christian Jaeger <ch@christianjaeger.ch>
2016-05-21 22:45:03 +0100

    fortran: 'subroutine' does not allow for recursion
    $ gfortran foo.f95 && ./a.out
    foo.f95:3.18:
    
      call fib(n-1, a)
                      1
    Error: SUBROUTINE 'fib' at (1) cannot be called recursively, as it is not RECURSIVE
    foo.f95:4.18:
    
      call fib(n-2, b)
                      1
    Error: SUBROUTINE 'fib' at (1) cannot be called recursively, as it is not RECURSIVE

--- meeting-1/foo.f95
+++ meeting-1/foo.f95
@@ -1,5 +1,13 @@
+subroutine fib(n,res)
+  integer :: n,res,a,b
+  call fib(n-1, a)
+  call fib(n-2, b)
+  res=a+b
+end subroutine fib
+
 program foo
-real :: a
-a = 1.5
-print *, a
+integer :: a,aa
+a = 30
+call fib(a,aa)
+print *, aa
 end program foo
