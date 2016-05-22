87e5abdbaacb8a731bb068191529afa1a563fd01
Christian Jaeger <ch@christianjaeger.ch>
2016-05-22 00:28:39 +0000

    (hmm, no overflow check for uint, also, why stack so big?)
     $ ./foo -12
      runtime: goroutine stack exceeds 1000000000-byte limit
     fatal error: stack overflow
    
     runtime stack:
     runtime.throw(0x506d00, 0xe)
    	 /usr/lib/go/src/runtime/panic.go:530 +0x90
     runtime.newstack()
    	 /usr/lib/go/src/runtime/stack.go:940 +0xb11
     runtime.morestack()
    	 /usr/lib/go/src/runtime/asm_amd64.s:359 +0x7f
    
     goroutine 1 [stack growth]:
     main.fib(0xffffffffff000020, 0x0)
    	 /home/chris/julia-experiments/meeting-1/Go/foo.go:10 fp=0xc8400c22d8 sp=0xc8400c22d0
     main.fib(0xffffffffff000021, 0x0)
    	 /home/chris/julia-experiments/meeting-1/Go/foo.go:11 +0x24 fp=0xc8400c22f8 sp=0xc8400c22d8
     main.fib(0xffffffffff000022, 0x0)
    
    ~same for
    
     $ ./foo 12

--- meeting-1/Go/foo.go
+++ meeting-1/Go/foo.go
@@ -3,14 +3,18 @@ package main
 import (
 	"os"
 	"strconv"
+	"log"
 	"fmt"
 )
 
-func fib (n int) int {
+func fib (n uint) int {
 	return fib(n-1) + fib(n-2)
 }
 
 func main () {
 	n, err := strconv.Atoi(os.Args[1])
-	fmt.Println("fib(", n, ") = ", fib(n))
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("fib(", n, ") = ", fib(uint(n)))
 }
