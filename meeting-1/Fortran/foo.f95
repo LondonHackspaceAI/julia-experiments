e5c962c380e66335b9d99f49d465779bfb3be09c
Christian Jaeger <ch@christianjaeger.ch>
2016-05-29 18:27:51 +0100

    what's the problem?
     $ gfortran -Wall foo.f95
     foo.f95:7.12:
    
     print *, fib(30)
    	     1
     Error: Return type mismatch of function 'fib' at (1) (REAL(4)/INTEGER(4))

--- meeting-1/Fortran/foo.f95
+++ meeting-1/Fortran/foo.f95
@@ -1,15 +1,17 @@
 program foo
-integer :: a,aa
-a = 30
-aa= fib(a)
-print *, aa
+integer :: a
+integer :: aa
+! a = 30
+! aa= fib(a)
+! print *, aa
+print *, fib(30)
 end program foo
 
 recursive function fib(n) result(res)
   integer :: n
   integer :: res
   if (n < 2) then
-     res=1
+     res= 1
   else
      res= fib(n-1) + fib(n-2)
   end if
