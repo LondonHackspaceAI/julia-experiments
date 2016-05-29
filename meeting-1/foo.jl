4a895935c67836761538113a1384cda317a57a5f
Christian Jaeger <ch@christianjaeger.ch>
2016-05-29 17:46:37 +0000

    cleanup
    single_fib:
    
    chris@borg:~/julia-experiments/meeting-1$ time julia -O -p 64 foo.jl  40
    40
    165580141
    
    real	0m56.026s
    user	3m15.932s
    sys	12m21.056s
    
    parallel_fib:
    
    chris@borg:~/julia-experiments/meeting-1$ time julia -O -p 64 foo.jl  40
    40
    165580141
    
    real	1m5.637s
    user	8m30.988s
    sys	8m22.900s

--- meeting-1/foo.jl
+++ meeting-1/foo.jl
@@ -1,37 +1,28 @@
-c=0
+function single_fib(n::Int)
+    _fib=single_fib
+    n < 2 ? 1 : _fib(n-1) + _fib(n-2)
+end
 
-@everywhere function _fib(n::Int, level::Int)
-    fib_(n::Int)= _fib(n, level+1)
-    #global c=c+1
+@everywhere function _parallel_fib(n::Int, level::Int)
+    _fib(n::Int)= _parallel_fib(n, level+1)
     if n < 2
 	1
     else
-        if (level<6) # 2**6==64 cores
-            a= @spawn fib_(n-1)
-            b= fib_(n-2)
+        # 2**6==64 already, but not all of them run long enough.
+        if (level<11)
+            a= @spawn _fib(n-1)
+            b= _fib(n-2)
             fetch(a) + b
         else
-            fib_(n-1) + fib_(n-2)
+            _fib(n-1) + _fib(n-2)
         end
     end
 end
 
-function simple_fib(n::Int)
-    #global c=c+1
-    if n < 2
-	1
-    else
-        simple_fib(n-1) + simple_fib(n-2)
-    end
-end
+parallel_fib(n)= _parallel_fib(n, 0)
 
-#fib(n)= _fib(n, 0)
-fib(n)= simple_fib(n)
+fib=parallel_fib
 
 n = parse(Int,ARGS[1])
 println(n)
 println(fib(n))
-print("c:")
-println(c)
-
-#println(@spawn fib(n))
